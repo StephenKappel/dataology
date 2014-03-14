95607ee70010337662b791f015bd578003b7268d
StephenKappel <stephen.kappel@outlook.com>
2014-03-13 23:56:42 -0400

    updated R code with new data model

--- Forums_Project/R Code/Prelim.R
+++ Forums_Project/R Code/Prelim.R
@@ -1,29 +1,37 @@
 library(RODBC)
+library(ggplot2)
 
 #create connection to database
-myDB = odbcConnect("DataHogDB")
+myDB = odbcConnect("LocalSQL")
 
 #query the database and put results in a dataframe
-threads = sqlQuery(myDB, "SELECT * FROM Thread_Basics WHERE Display_Name = 'SQL Server Express'", stringsAsFactors=FALSE)
+forums = sqlQuery(myDB, "SELECT * FROM Forums.dbo.Forum_Summary", stringsAsFactors=FALSE)
 
-#make sure data is loaded as expected
-head(threads)
+leng#make sure data is loaded as expected
+head(forums)
 
-#get summary data about creation dates
-summary(threads$Created_On)
-summary(as.POSIXlt(threads$Created_On)$mon)
+#plot MTTR vs. num contributors
+plot(forums$Num_Unique_Contributors, forums$Mean_Time_To_Answer, main="MTTR vs. Unique Contributors", xlab="Unique Contributors in 2013", ylab="Mean Time to Resolution (days)")
+#plot MTTR vs. num contributors for forums with more than 100 threads
+with(forums[forums$Num_Threads >= 100,], plot(Num_Unique_Contributors, Mean_Time_To_Answer, main="MTTR vs. Unique Contributors for Forums with >= 100 Threads", xlab="Unique Contributors in 2013", ylab="Mean Time to Resolution (days)"))
+reg = with(forums[forums$Num_Threads >= 100,],lm(Mean_Time_To_Answer~Num_Unique_Contributors))
+abline(reg)
 
-#create a histogram to see # of threads created each day of the week
-#observation: as expected, lowest volume on weekends, highest volume on Wednedsay
-wbins=seq(0,7,by=1)
-hist(as.POSIXlt(threads$Created_On)$wday + 1, main = "Threads by Week Day", xlab = "Day of Week", breaks=wbins, labels = TRUE)
 
-##create a histogram to see # of threads created each month of year
-#observation: sudden drop off in volume in 3rd quarter
-mbins=seq(0,12,by=1)
-hist(as.POSIXlt(threads$Created_On)$mon + 1, main = "Threads by Month", xlab = "Month", breaks=mbins, labels = TRUE)
-
-##plot a scatter of views vs. day of week posted
-#observation: nothing particularly interesting; there are a few posts that really stand out from the pack
-plot(jitter(as.POSIXlt(threads$Created_On)$wday + 1),  threads$Views, xlab = "Day of Week", ylab = "Views",main = "Views by Creation Day", log="y")
-
+# #get summary data about creation dates
+# summary(threads$Created_On)
+# summary(as.POSIXlt(threads$Created_On)$mon)
+# 
+# #create a histogram to see # of threads created each day of the week
+# #observation: as expected, lowest volume on weekends, highest volume on Wednedsay
+# wbins=seq(0,7,by=1)
+# hist(as.POSIXlt(threads$Created_On)$wday + 1, main = "Threads by Week Day", xlab = "Day of Week", breaks=wbins, labels = TRUE)
+# 
+# ##create a histogram to see # of threads created each month of year
+# #observation: sudden drop off in volume in 3rd quarter
+# mbins=seq(0,12,by=1)
+# hist(as.POSIXlt(threads$Created_On)$mon + 1, main = "Threads by Month", xlab = "Month", breaks=mbins, labels = TRUE)
+# 
+# ##plot a scatter of views vs. day of week posted
+# #observation: nothing particularly interesting; there are a few posts that really stand out from the pack
+# plot(jitter(as.POSIXlt(threads$Created_On)$wday + 1),  threads$Views, xlab = "Day of Week", ylab = "Views",main = "Views by Creation Day", log="y")
---
+++ Forums_Project/SQL Queries/User Summary.sql
@@ -0,0 +1,9 @@
+-- User Summary
+
+-- Num_Forums
+-- Num_Categories
+-- TTR of their questions
+-- Number of answers provided
+-- Num Votes
+-- Num Posts
+-- clustering of questions answers in time periods
