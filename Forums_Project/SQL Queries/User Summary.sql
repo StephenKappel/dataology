d216e5ebb7c1da1d0e00b22b189476b5babb3050
StephenKappel <stephen.kappel@outlook.com>
2014-04-12 16:45:12 -0400

    Clean up; first push of knitr doc

--- Forums_Project/SQL Queries/User Summary.sql
+++
@@ -1,9 +0,0 @@
--- User Summary
-
--- Num_Forums
--- Num_Categories
--- TTR of their questions
--- Number of answers provided
--- Num Votes
--- Num Posts
--- clustering of questions answers in time periods
